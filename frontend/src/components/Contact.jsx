b9537175a880a097dc2cafab0b3aa322b22d54f6
Ankita Modak <ankitamodak@gmail.com>
2024-09-08 14:44:56 +0530

    lastone commit

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "https://event-planning-backend-wn0k.onrender.com/api/v1/message/send",
+        "https://mern-stack-event-planning-project.onrender.com/api/v1/message/send",
         {
           name,
           email,
