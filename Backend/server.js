1241bdba2b951f26ad6c9175933eb09810e2c871
ANKITAMODAK109 <ankitamodak109@gmail.com>
2024-09-08 17:51:25 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,5 @@
 import app from "./app.js";
 
-const app = express()
-const port = process.env.PORT || 4000;
-
 app.listen(process.env.PORT, ()=>{
     console.log(`server listening at port ${process.env.PORT}`);
 });
